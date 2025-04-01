fed3faa76b0a4c9cfa26bf47bc2eecf54b829206
renato albuquerque <renatomalb@hotmail.com>
2025-03-31 22:14:07 -0300

    updated sql file

--- sql/kpis_analysis.sql
+++ sql/kpis_analysis.sql
@@ -3,6 +3,7 @@ select * from public.dim_departamento;
 select * from public.dim_divisao;
 select * from public.dim_funcionario;
 select * from public.fat_folha_pagamento;
+select * from public.dim_calendar;
 
 --
 -- KPIS
@@ -25,19 +26,27 @@ where dc.nk_dim_cargo <> 0
 group by ds_dim_cargo
 order by media_salarial desc;
 
--- MÉDIA TEMPO DE EMPRESA
-
 -- CUSTO TOTAL FOLHA DE PAGAMENTO
+select sum(vl_fat_folha_pagamento_salario)::numeric(18,2) as custo_total_folha_pagamento
+from fat_folha_pagamento
+order by custo_total_folha_pagamento desc;
 
 --
 -- ANÁLISES
 --
 
--- FUNCIONÁRIOS COM MAIOR TEMPO DE EMPRESA
-
 -- DISTRIBUIÇÃO SALARIAL POR CARGO
+select dc.ds_dim_cargo as cargo, fat.vl_fat_folha_pagamento_salario::numeric(18,2) as salario
+from fat_folha_pagamento fat
+inner join dim_cargo dc on fat.sk_dim_cargo = dc.sk_dim_cargo
+where dc.nk_dim_cargo <> 0
+group by ds_dim_cargo, salario
+order by salario desc;
 
 -- EVOLUÇÃO DO CUSTO DE FOLHA DE PAGAMENTO
-
-
+select dca.nr_dim_calendar_month as mes, dca.nr_dim_calendar_year as ano, fat.vl_fat_folha_pagamento_salario::numeric(18,2) as custo_folha_pagamento
+from fat_folha_pagamento fat
+inner join dim_calendar dca on fat.id_dim_calendar = dca.id_dim_calendar
+group by dca.nr_dim_calendar_month, dca.nr_dim_calendar_year, fat.vl_fat_folha_pagamento_salario
+order by dca.nr_dim_calendar_month, dca.nr_dim_calendar_year;
 
